cf3e5ef88b7a4bc27895b1948036de11270622f4
Charlie Welsh <charlie@allotrope.xyz>
2020-11-19 06:50:11 +0000

    Put everything in functions

--- loopFun.cpp
+++ loopFun.cpp
@@ -1,50 +1,70 @@
 #include <iostream>
 #include <iomanip>
 
-int main()
-{
-
-	int boxHeight = 0;
-	int boxWidth = 0;
-
-	std::cout << "Enter a box height (it should be between 3 and 10): ";
-	std::cin >> boxHeight; 
-
-	while (3 > boxHeight || boxHeight > 10) {
-		std::cout << "That's out of bounds (it should be between 3 and 10): ";
-		std::cin >> boxHeight; 
+int getInteger(int low, int high){
+	int userInput = 0;
+	
+	std::cout << "Enter an integer (it should be between " << low << " and " <<  high << "): ";
+	std::cin >> userInput; 
+	
+	while (low > userInput || userInput > high) {
+		std::cout << "That number is out of range (it should be between " << low << " and " <<  high << "): ";
+		std::cin >> userInput; 
 	}
 	
-	std::cout << "Enter a box width (it should be between " << boxHeight + 1 << " and 20): ";
-	std::cin >> boxWidth; 
-	
-	while (boxHeight > boxWidth || boxWidth > 20) {
-		std::cout << "That's out of bounds (it should be between " << boxHeight + 1 << " and 20): ";
-		std::cin >> boxWidth; 
+	return userInput;
+}
+
+void printRange(int low, int high){
+	std::cout << "The integers from " << low << " to " << high << " are:" << std::endl << "\t";
+	for (int i = low; i <= high; i++) {
+		std::cout << i << " ";
 	}
-	
+	std::cout << std::endl;
+}
+
+void printAvg(int low, int high) {
 	int rangeSum = 0;
 	int rangeLength = 0;
-	
-	std::cout << "The integers from " << boxHeight << " to " << boxWidth << " are:" << std::endl << "\t";
-	for (int i = boxHeight; i <= boxWidth; i++) {
-		std::cout << i << " ";
+	for (int i = low; i <= high; i++) {
 		// Sums up the contents of the range. 
 		rangeSum += i;
 		// Increases the total length of this range by 1 each time, resulting in the total length of the range.
 		rangeLength += 1;
 	}
-	std::cout << std::endl << "and the average of those numbers is " << (float)rangeSum / (float)rangeLength << std::endl << std::endl; 
-	
-	std::cout << std::setfill('*') << std::setw(boxWidth) << "" << std::endl;
-	for (int i = 0; i < boxHeight -2; i++) {
-		std::cout << std::setw(boxWidth - 2) << std::left << std::setfill(' ') << "*" << std::internal << " " << std::right << "*" << std::endl;
+	std::cout << "and the average of those numbers is " << (float)rangeSum / (float)rangeLength << std::endl << std::endl; 
+}
+
+void printBox(int height, int width){
+	std::cout << std::setfill('*') << std::setw(width) << "" << std::endl;
+	for (int i = 0; i < height -2; i++) {
+		std::cout << std::setw(width - 2) << std::left << std::setfill(' ') << "*" << std::internal << " " << std::right << "*" << std::endl;
 	}
-	std::cout << std::setfill('*') << std::setw(boxWidth) << "" << std::endl << std::endl;
-	
-	for (int i = 0; i <= boxHeight; i ++) {
+	std::cout << std::setfill('*') << std::setw(width) << "" << std::endl << std::endl;
+}
+
+void printWedge(int height){
+	for (int i = 0; i <= height; i ++) {
 		std::cout << std::setfill('*') << std::setw(i * 2) << "" << std::endl;
 	}
+}
+
+int main()
+{
+	int boxHeight = getInteger(3, 10);
+	int boxWidth = getInteger(boxHeight+1, 20);
+
+	// Formatting
+	std::cout << std::endl;
+
+	printRange(boxHeight, boxWidth);
 	
+	printAvg(boxHeight, boxWidth);
+	
+	printBox(boxHeight, boxWidth);
+	
+	printWedge(boxHeight);
+	
+	// Formatting
 	std::cout << std::endl;
 }
